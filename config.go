8826bbdf9cb0b99041cfa00fe4662203d26a0c3d
Ian Spence <ian@ecnepsnai.com>
2022-01-21 17:46:06 -0800

    Formatting improvements

--- config.go
+++ config.go
@@ -8,45 +8,35 @@ import (
 )
 
 func loadConfig(fileName string, obj interface{}) bool {
-	if fileExists(fileName + ".json") {
-		f, err := os.Open(fileName + ".json")
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
+	var f *os.File
+	var err error
+	isJSON := false
 
+	if fileExists(fileName + ".json") {
+		f, err = os.Open(fileName + ".json")
+		isJSON = true
+	} else if fileExists(fileName + ".yml") {
+		f, err = os.Open(fileName + ".yml")
+	} else if fileExists(fileName + ".yaml") {
+		f, err = os.Open(fileName + ".yaml")
+	} else {
+		return false
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	if isJSON {
 		if err := json.NewDecoder(f).Decode(obj); err != nil {
 			panic(err)
 		}
-
-		return true
-	} else if fileExists(fileName + ".yml") {
-		f, err := os.Open(fileName + ".yml")
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-
-		if err := yaml.NewDecoder(f).Decode(obj); err != nil {
-			panic(err)
-		}
-
-		return true
-	} else if fileExists(fileName + ".yaml") {
-		f, err := os.Open(fileName + ".yaml")
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-
-		if err := yaml.NewDecoder(f).Decode(obj); err != nil {
-			panic(err)
-		}
-
 		return true
 	}
-
-	return false
+	if err := yaml.NewDecoder(f).Decode(obj); err != nil {
+		panic(err)
+	}
+	return true
 }
 
 func fileExists(filePath string) bool {
